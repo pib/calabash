f091e231670376dc525696a6d0ef883672b0da7c
Paul Bonser <pib@paulbonser.com>
2011-03-12 14:05:56 -0600

    Fix concat_lines function so description comes out right

--- feature.lua
+++ feature.lua
@@ -9,15 +9,15 @@ local space = locale.space
 local newline = S"\n" + S"\r\n"
 
 function concat_lines(...)
-   table.concat(..., '\n')
+   return table.concat(..., '\n')
 end
 
 G = lpeg.Ct{
    "Feature",
-   Feature = P"Feature:" * space^0 * (lpeg.Cg((1 - newline)^1, 'name')) * (newline * V'Description')^0,
-   Description = lpeg.Cg(lpeg.Ct(V'PlainLine'^1 * newline^0) / concat_lines,
+   Feature = P"Feature:" * space^0 * (lpeg.Cg((1 - newline)^1, 'name')) * (newline * V'Description')^-1,
+   Description = lpeg.Cg(lpeg.Ct(V'PlainLine'^1) / concat_lines,
                          'description'),
-   PlainLine = ((space^0) * lpeg.Cg((1 - newline)^1) * newline^-1)
+   PlainLine = (space - newline)^0 * lpeg.Cg((1 - newline)^1) * newline
 }
 
 function parse(feature_string)
